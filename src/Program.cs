468a9c3a11f18c8ccbf7f7e51b6916ba60ac80cf
David Fowler <davidfowl@gmail.com>
2018-07-05 02:22:07 -0700

    Fix bug with observing too much

--- src/Program.cs
+++ src/Program.cs
@@ -72,7 +72,7 @@ namespace TcpEcho
                         buffer = buffer.Slice(buffer.GetPosition(1, position.Value));
                     }
 
-                    reader.AdvanceTo(buffer.Start, buffer.End);
+                    reader.AdvanceTo(buffer.Start);
 
                     if (result.IsCompleted)
                     {
