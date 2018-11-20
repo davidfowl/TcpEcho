d58798f35f23da4becb29f57038c96eedf726467
Pavel Krymets <pavel@krymets.com>
2018-11-19 21:02:43 -0800

    Add socket server and client (#10)
    * Add socket server and client
    
    * sln

---
+++ src/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TcpEcho
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Connecting to port 8087");
+
+            clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 8087));
+
+            var buffer = new byte[1];
+            using (var stream = new NetworkStream(clientSocket))
+            {
+                while (true)
+                {
+                    buffer[0] = (byte)Console.Read();
+                    await stream.WriteAsync(buffer, 0 ,1);
+                }
+            }
+        }
+    }
+}
---
+++ src/SocketServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpEcho
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var listenSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 8087));
+
+            Console.WriteLine("Listening on port 8087");
+
+            listenSocket.Listen(120);
+
+            while (true)
+            {
+                var socket = await listenSocket.AcceptAsync();
+                _ = ProcessLinesAsync(socket);
+            }
+        }
+
+        private static async Task ProcessLinesAsync(Socket socket)
+        {
+            Console.WriteLine($"[{socket.RemoteEndPoint}]: connected");
+
+            using (var stream = new NetworkStream(socket))
+            using (var reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    ProcessLine(socket, await reader.ReadLineAsync());
+                }
+            }
+
+            Console.WriteLine($"[{socket.RemoteEndPoint}]: disconnected");
+        }
+
+        private static void ProcessLine(Socket socket, string s)
+        {
+            Console.Write($"[{socket.RemoteEndPoint}]: ");
+            Console.WriteLine(s);
+        }
+    }
+}
